93807ef920c5fd7188986374031925ba3fdddff0
elnormous <elviss@elviss.lv>
2016-09-06 02:52:32 +0300

    Update Ouzel

--- src/main.cpp
+++ src/main.cpp
@@ -14,5 +14,7 @@ void ouzelMain(const std::vector<std::string>& args)
     settings.resizable = true;
     settings.sampleCount = 4;
     settings.textureFiltering = ouzel::graphics::Renderer::TextureFiltering::TRILINEAR;
-    engine.init(settings, std::bind(&SpineSample::run, &sample));
+    engine.init(settings);
+
+    sample.run();
 }
